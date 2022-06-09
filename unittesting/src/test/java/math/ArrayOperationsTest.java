b8e516864a5b4b2b3c44cc2abc4bb181e85c2437
aristotelis gkithkopoulos <aristogithk@gmail.com>
2022-06-09 20:14:29 +0300

    modify mock test

--- unittesting/src/test/java/math/ArrayOperationsTest.java
+++ unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -7,12 +7,13 @@ import static org.mockito.Mockito.*;
 
 public class ArrayOperationsTest {
     ArrayOperations ao = new ArrayOperations();
-    
+
     @Test
     public void testPrimeNumbers() {
         String resource = "src/test/resources/test_mock.txt";
         MyMath m = mock(MyMath.class);
         FileIO file = mock(FileIO.class);
+        when(file.readFile(resource)).thenReturn(new int [] {2, 3, 8});
         when(m.isPrime(2)).thenReturn(true);
         when(m.isPrime(3)).thenReturn(true);
         when(m.isPrime(8)).thenReturn(false);
