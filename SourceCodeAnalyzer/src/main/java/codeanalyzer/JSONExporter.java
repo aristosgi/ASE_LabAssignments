09fbde2dd9a7f996a67d420950d7970514d03b82
aristotelis gkithkopoulos <aristogithk@gmail.com>
2022-06-15 22:23:19 +0300

    Modify Metrics exporter with strategy pattern

---
+++ SourceCodeAnalyzer/src/main/java/codeanalyzer/JSONExporter.java
@@ -0,0 +1,8 @@
+public class JSONExporter {
+
+    private void writeJson(Map<String, Integer> metrics, String filepath) {
+		// Functionality not implemented yet
+		// No need to implement it for the assignment
+	}
+    
+}
--- SourceCodeAnalyzer/src/main/java/codeanalyzer/MetricsExporter.java
+++ SourceCodeAnalyzer/src/main/java/codeanalyzer/MetricsExporter.java
@@ -24,31 +24,5 @@ public class MetricsExporter {
 		}
 	}
 	
-	private void writeCsv(Map<String, Integer> metrics, String filepath) {
-		File outputFile = new File(filepath + ".csv");
-		StringBuilder metricsNames = new StringBuilder();
-		StringBuilder metricsValues = new StringBuilder();
-		
-		for (Map.Entry<String, Integer> entry : metrics.entrySet()) {
-			metricsNames.append(entry.getKey() + ",");
-			metricsValues.append(entry.getValue()+",");
-		}
-		
-		try {
-			FileWriter writer = new FileWriter(outputFile);
-			writer.append(metricsNames + "\n");
-			writer.append(metricsValues + "\n");
-			writer.close();
-			System.out.println("Metrics saved in " + outputFile.getAbsolutePath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private void writeJson(Map<String, Integer> metrics, String filepath) {
-		// Functionality not implemented yet
-		// No need to implement it for the assignment
-	}
 
 }
